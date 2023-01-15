5ccee5208ddad0861cdc5d6a3b9db9b5417cc8dc
riddhipatel09 <116479348+riddhipatel09@users.noreply.github.com>
2023-01-16 01:14:48 +0530

    Create switch_case.cpp

---
+++ Basics of cpp/switch_case.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   char inp;
+   cin>>inp;
+   switch(inp){
+       case 'a':
+       cout<<"Tirth";
+       break;
+       
+       case 'b':
+       cout<<"Riddhi";
+       break;
+       
+       default:
+       cout<<"Other";
+   }
+  
+  
+
+    return 0;
+}
