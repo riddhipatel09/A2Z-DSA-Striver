18d7cd874cb7824bd197b629915737fc7a1fb820
riddhipatel09 <116479348+riddhipatel09@users.noreply.github.com>
2023-09-08 19:25:56 +0530

    Create gcd\HCF.cpp

---
+++ basic_maths.cpp folder/gcd\HCF.cpp
@@ -0,0 +1,14 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int num1 = 4, num2 = 8;
+	int ans;
+	for (int i = 1; i <= min(num1, num2); i++) {
+		if (num1 % i == 0 && num2 % i == 0) {
+			ans = i;
+		}
+	}
+	cout << "The GCD of the two numbers is "<<ans;
+}
