03f917d9ac6717dd4a8daf2c67fb6e09d318bb93
riddhipatel09 <116479348+riddhipatel09@users.noreply.github.com>
2024-01-29 06:53:37 +0530

    Update palindrome.cpp

--- Basic recursion.cpp/palindrome.cpp
+++ Basic recursion.cpp/palindrome.cpp
@@ -1,42 +1,28 @@
-//Time Complexity:  O(N)
-//Space Complexity: O(1)
+//Time Complexity: O(N) { Precisely, O(N/2) as we compare the elements N/2 times and swap them}.
+//Space Complexity: O(1) { The elements of the given array are swapped in place so no extra space is required}.
 
-#include<bits/stdc++.h>
-
+#include <iostream>
 using namespace std;
 
-bool isPalindrome(string s) {
-
-        int left = 0, right = s.length()-1;
-        while(left<right)
-        {
-            if(!isalnum(s[left])) 
-                left++;
-            else if(!isalnum(s[right])) 
-                right--;
-            else if(tolower(s[left])!=tolower(s[right])) 
-                return false;
-            else {
-                left++; 
-                right--;
-            }
-        }
-        return true;
-
+bool palindrome(int i, string& s){
+    
+    // Base Condition
+    // If i exceeds half of the string means all the elements 
+    // are compared, we return true.
+    if(i>=s.length()/2) return true;
+    
+    // If the start is not equal to the end, not the palindrome.
+    if(s[i]!=s[s.length()-i-1]) return false;
+    
+    // If both characters are the same, increment i and check start+1 and end-1.
+    return palindrome(i+1,s);
 }
+
 int main() {
-
-  string str = "ABCDCBA";
-  bool ans = isPalindrome(str);
-
-  if (ans == true) {
-    cout << "Palindrome";
-  } else {
-    cout << "Not Palindrome";
-  }
-  return 0;
+	
+	// Example string.
+	string s = "madam";
+	cout<<palindrome(0,s);
+	cout<<endl;
+	return 0;
 }
-Output: Palindrome
-
-
-
