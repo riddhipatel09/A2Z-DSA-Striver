7c676309ca3c4685f9f2db57dee136350b181e8b
riddhipatel09 <116479348+riddhipatel09@users.noreply.github.com>
2024-03-10 19:29:47 +0530

    Create 231. Power of Two.cpp

---
+++ Bit manipulation/231. Power of Two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n == 0){
+            return false;
+        }
+        return floor(log2(n))==ceil(log2(n))?true:false;
+        
+    }
+};
+
+TC = 0(1)
